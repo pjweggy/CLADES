597625de5286d90bb814c5291fd335019ad9f32c
FatherOfEgg <fatherofegg@gmail.com>
2025-09-05 15:03:31 -0400

    Refactored some parts to better follow PEP 8

--- CLADES.py
+++ CLADES.py
@@ -9,7 +9,12 @@ import sys
 from pathlib import Path
 import argparse
 
-__all__ = ["compute_summary_statistics", "species_delimitation", "compute_best_assignment", "get_species_names", "get_class_labels", "write_sumstat", "write_total_sumstat"]
+__all__ = [
+    "compute_summary_statistics", "species_delimitation",
+    "compute_best_assignment", "get_species_names",
+    "get_class_labels", "write_sumstat", "write_total_sumstat"
+]
+
 
 def compute_summary_statistics(seq_data, nbin=3, delim=r'\^'):
     """
@@ -46,6 +51,7 @@ def compute_summary_statistics(seq_data, nbin=3, delim=r'\^'):
 
     return SS
 
+
 def species_delimitation(SS, output_dir, model):
     """
     Perform species delimitation for pairwise populations.
@@ -64,22 +70,22 @@ def species_delimitation(SS, output_dir, model):
 
         subprocess.run(
             f"svm-scale -r {model}.range {sumstat_filepath} > {sumstat_filepath}.scale",
-            shell = True,
-            stdout = subprocess.PIPE
+            shell=True,
+            stdout=subprocess.PIPE
         )
 
         key_out_filepath = output_dir / f"{key}.out"
         subprocess.run(
             f"svm-predict -b 1 -q {sumstat_filepath}.scale {model}.sumstat.scale.model {key_out_filepath}",
-            shell = True,
-            stdout = subprocess.PIPE
+            shell=True,
+            stdout=subprocess.PIPE
         )
 
-        Out = np.loadtxt(key_out_filepath, comments = "labels")
+        Out = np.loadtxt(key_out_filepath, comments="labels")
         if len(Out.shape) == 1:
             Out = Out.reshape(1, Out.shape[0])
 
-        res = np.mean(Out, axis = 0)[1:3]
+        res = np.mean(Out, axis=0)[1:3]
         Res[key] = res
 
     seq_out_filepath = output_dir / f"{seq_data.name}.out"
@@ -89,6 +95,7 @@ def species_delimitation(SS, output_dir, model):
             f.write(f"{key} {Res[key][0]:.4f} {Res[key][1]:.4f}\n")
 
     return Res
+
 
 def compute_best_assignment(Res):
     """
@@ -126,6 +133,7 @@ def compute_best_assignment(Res):
 
     return clusters, cluster, prob
 
+
 def get_species_names(SS):
     """
     Get the species names from summary statistics.
@@ -144,6 +152,7 @@ def get_species_names(SS):
         species_names.add(specie2)
 
     return sorted(list(species_names))
+
 
 def get_class_labels(species_names):
     """
@@ -166,10 +175,11 @@ def get_class_labels(species_names):
 
     return class_labels
 
+
 def write_sumstat(SS, output_dir):
     """
     Writes summary statistics to a file named {key}.sumstat in the specified output directory.
-    
+
     Args:
         SS: Summary statistics.
         output_dir: Directory where the summary statistics file will be saved.
@@ -179,10 +189,11 @@ def write_sumstat(SS, output_dir):
         with sumstat_filepath.open("w") as f:
             f.write(SS[key])
 
+
 def write_total_sumstat(SS, output_dir):
     """
     Writes all summary statistics to a single file named total.sumstat in the specified output directory.
-    
+
     Args:
         SS: Summary statistics.
         output_dir: Directory where the total summary statistics file will be saved.
@@ -203,9 +214,11 @@ def write_total_sumstat(SS, output_dir):
             for i, sumstat in enumerate(group):
                 f.write(f"{class_labels[i]} {sumstat}\n")
 
+
 def RecogIndv(line, delim):
     pattern = fr"[A-Za-z]+[0-9]+{delim}[A-Za-z0-9]+"
     return line if re.match(pattern, line) else ""
+
 
 def Process(rawdata, delim, nbin, SS):
     if len(rawdata) != 0:
@@ -220,7 +233,7 @@ def Process(rawdata, delim, nbin, SS):
         for i in range(len(popsize) - 1):
             for j in range(i + 1, len(popsize)):
                 pos = list(chain(range(startpoint[i], startpoint[i] + popsize[i]), range(startpoint[j], startpoint[j] + popsize[j])))
-                hap = Allmat[pos,:]
+                hap = Allmat[pos, :]
                 sumstat = CollectSS2pop(hap, popsize[i], popsize[j], nbin, nsites, snplist)
                 key = f"{spn[i]}-{spn[j]}"
 
@@ -231,6 +244,7 @@ def Process(rawdata, delim, nbin, SS):
 
     return SS
 
+
 def AddSS(sumstat):
     s = ""
 
@@ -240,25 +254,26 @@ def AddSS(sumstat):
 
     return s
 
+
 def CollectSS2pop(hap, popsize1, popsize2, nbin, nsites, snplist):
-    hap1 = hap[0:popsize1,:]
-    hap2 = hap[popsize1:,:]
+    hap1 = hap[0:popsize1, :]
+    hap2 = hap[popsize1:, :]
     sfs = SFS_fold_bin(hap, nbin, nsites)
-    #sfs=SFS_fold_bin(hap,nbin)
-    #pdmean,pdstd=PairDiff(hap)
+    # sfs=SFS_fold_bin(hap,nbin)
+    # pdmean,pdstd=PairDiff(hap)
     pdmean1 = PairDiff(hap1)
     pdmean2 = PairDiff(hap2)
     pdmean3 = PairDiff2(hap1, hap2)
     pdmean4 = (pdmean1 + pdmean2) / 2
     n_prvpos = prvpos(hap, popsize1, popsize2, nsites)
     lsubseq = Lsubseq(hap, popsize1, popsize2, nsites, snplist)
-    #n_prvpos=prvpos(hap,popsize1,popsize2)
-    #lsubseq=Lsubseq(hap,popsize1,popsize2)
+    # n_prvpos=prvpos(hap,popsize1,popsize2)
+    # lsubseq=Lsubseq(hap,popsize1,popsize2)
 
     if pdmean4 == 0:
         ktheta = 2
     else:
-        ktheta =pdmean3 / pdmean4
+        ktheta = pdmean3 / pdmean4
 
     if pdmean3 == 0:
         Fst = 0
@@ -266,11 +281,12 @@ def CollectSS2pop(hap, popsize1, popsize2, nbin, nsites, snplist):
         Fst = (pdmean3 - pdmean4) / pdmean3
     ss = np.hstack((sfs, ktheta))
     ss = np.hstack((ss, Fst))
-    #ss=np.hstack((ss,pdmean4))
+    # ss=np.hstack((ss,pdmean4))
     ss = np.hstack((ss, n_prvpos))
     ss = np.hstack((ss, lsubseq))
 
     return ss
+
 
 def SFS_fold(hap, nsites):
     if isinstance(hap, list):
@@ -286,16 +302,17 @@ def SFS_fold(hap, nsites):
         sfs = np.zeros(hap_dim[0] // 2 + 1)
 
     for i in range(hap_dim[1]):
-        temp = (hap[:,i] == 1).sum()
+        temp = (hap[:, i] == 1).sum()
         if temp <= (hap_dim[0] / 2):
             sfs[int(temp)] += 1
         else:
             sfs[hap_dim[0] - int(temp)] += 1
 
     return sfs / hap_dim[1]
-    #return sfs/nsites
+    # return sfs/nsites
 
-###Folded SFS with bin
+
+# Folded SFS with bin
 def SFS_fold_bin(hap, nbin, nsites):
     sfs = SFS_fold(hap, nsites)
     nlen = sfs.shape[0]
@@ -310,23 +327,25 @@ def SFS_fold_bin(hap, nbin, nsites):
 
     return sfs_bin
 
-###Intro-population Pairwise difference
+
+# Intro-population Pairwise difference
 def PairDiff(hap):
     hap_dim = hap.shape
 
     pairdiff = list()
     for i in range(hap_dim[0] - 1):
         for j in range(i + 1, hap_dim[0]):
-            pairdiff.append(pd(hap[i,:],hap[j,:]))
-    #print pairdiff
-    #pairdiff_reg=[x*(float(1.0)/hap_dim[1]) for x in pairdiff]  ##regularize pairwise difference
+            pairdiff.append(pd(hap[i, :], hap[j, :]))
+    # print pairdiff
+    # pairdiff_reg=[x*(float(1.0)/hap_dim[1]) for x in pairdiff]  ##regularize pairwise difference
     if len(pairdiff) > 0:
         pdmean = np.mean(pairdiff)
     else:
         pdmean = 0.0
 
-    #pdstd=std(pairdiff_reg)
-    return pdmean #,pdstd
+    # pdstd=std(pairdiff_reg)
+    return pdmean  # ,pdstd
+
 
 def pd(hap1, hap2):
     pd = 0
@@ -336,7 +355,8 @@ def pd(hap1, hap2):
 
     return pd
 
-###Outra-population Pairwise difference
+
+# Outra-population Pairwise difference
 def PairDiff2(hap1, hap2):
     hap1_dim = hap1.shape[0]
     hap2_dim = hap2.shape[0]
@@ -344,32 +364,34 @@ def PairDiff2(hap1, hap2):
 
     for i in range(hap1_dim):
         for j in range(hap2_dim):
-            pairdiff.append(pd(hap1[i,:],hap2[j,:]))
-    #print pairdiff
-    #pairdiff_reg=[x*(float(1.0)/hap1.shape[1]) for x in pairdiff]
+            pairdiff.append(pd(hap1[i, :], hap2[j, :]))
+    # print pairdiff
+    # pairdiff_reg=[x*(float(1.0)/hap1.shape[1]) for x in pairdiff]
     pdmean = np.mean(pairdiff)
-    #pdstd=std(pairdiff)
-    return pdmean#,pdstd
+    # pdstd=std(pairdiff)
+    return pdmean  # ,pdstd
 
-#percentage of private positions overall SNPs
+
+# percentage of private positions overall SNPs
 def prvpos(hap, popsize1, popsize2, nsites):
     hap_dim = hap.shape
     n_prvpos = 0
     for i in range(hap_dim[1]):
-        n0hap1 = (hap[0:popsize1,i] == 0).sum()
-        n1hap1 = (hap[0:popsize1,i] == 1).sum()
-        n0hap2 = (hap[popsize1:,i] == 0).sum()
-        n1hap2 = (hap[popsize1:,i] == 1).sum()
+        n0hap1 = (hap[0:popsize1, i] == 0).sum()
+        n1hap1 = (hap[0:popsize1, i] == 1).sum()
+        n0hap2 = (hap[popsize1:, i] == 0).sum()
+        n1hap2 = (hap[popsize1:, i] == 1).sum()
 
         if (n0hap1 == popsize1 or n1hap1 == popsize1) and (n0hap2 != popsize2 and n1hap2 != popsize2):
-            #print i+1
+            # print i+1
             n_prvpos += 1
         elif (n0hap2 == popsize2 or n1hap2 == popsize2) and (n0hap1 != popsize1 and n1hap1 != popsize1):
-            #print i+1
-            n_prvpos +=1
+            # print i+1
+            n_prvpos += 1
 
     return float(n_prvpos) / hap_dim[1]
-    #return float(n_prvpos)/nsites
+    # return float(n_prvpos)/nsites
+
 
 def Lsubseq(hap, popsize1, popsize2, nsites, snplist):
     tract = list()
@@ -379,9 +401,9 @@ def Lsubseq(hap, popsize1, popsize2, nsites, snplist):
     for i in range(popsize1):
         for j in range(popsize1, popsize1 + popsize2):
             nshare = 0
-            ##compare two sequences
+            # compare two sequences
             for k in range(nsnp):
-                if hap[i,k] == hap[j,k]:
+                if hap[i, k] == hap[j, k]:
                     if k == 0:
                         nshare += snplist[k] + 1
                     else:
@@ -390,7 +412,7 @@ def Lsubseq(hap, popsize1, popsize2, nsites, snplist):
                         tract.append(nshare)
                         nshare = 0
                 else:
-                    if k==0:
+                    if k == 0:
                         nshare += snplist[0]
                     else:
                         nshare += snplist[k] - snplist[k - 1] - 1
@@ -398,12 +420,12 @@ def Lsubseq(hap, popsize1, popsize2, nsites, snplist):
                     if k == nsnp - 1:
                         tract.append(nsites - snplist[-1])
                     nshare = 0
-            #print(tract)
+            # print(tract)
             lsubseq.append(max(tract))
             tract = list()
 
     Lsubseq = max(lsubseq)
-    #return float(Lsubseq)/hap.shape[1]
+    # return float(Lsubseq)/hap.shape[1]
     return float(Lsubseq) / nsites
 
 
@@ -417,6 +439,7 @@ def GetSP(popsize):
 
     return startpoint
 
+
 def Dict2Mat(rawdata, delim):
     speciesname = list()
     speciesinfo = dict()
@@ -424,8 +447,8 @@ def Dict2Mat(rawdata, delim):
     Allmat = []
 
     for key in sorted(rawdata):
-        ###process key
-        #r=re.compile('([a-d][1-2])([0-9]+)('+delim+')([A-D][1-2])')
+        # process key
+        # r=re.compile('([a-d][1-2])([0-9]+)('+delim+')([A-D][1-2])')
         r = re.compile('([A-Za-z]+[0-9]+)('+delim+')([A-Za-z0-9]+)')
         m = r.match(key)
         spn = m.group(3)
@@ -435,17 +458,18 @@ def Dict2Mat(rawdata, delim):
         else:
             speciesname.append(spn)
             speciesinfo[spn] = 1
-        ### process sequence
+        # process sequence
         if len(template) == 0:
             template = rawdata[key]
             Allmat = Gen01fromSeq(rawdata[key], template)
         else:
-            #Update(template,rawdata[key])
+            # Update(template,rawdata[key])
             Allmat = np.vstack((Allmat, Gen01fromSeq(rawdata[key], template)))
 
     popsize = [speciesinfo[spn] for spn in speciesname]
 
     return popsize, Allmat, speciesname
+
 
 def Seq2SNP(Allmat, rawdata):
     nr, nc = Allmat.shape
@@ -456,9 +480,10 @@ def Seq2SNP(Allmat, rawdata):
         print("Error Message:Gene has no variant! Please delete this gene or concatenate multiple genes.")
         sys.exit(1)
 
-    AllSNP = np.array(Allmat[:,snplist])
+    AllSNP = np.array(Allmat[:, snplist])
 #    print AllSNP.shape
     return AllSNP, snplist
+
 
 def Update(template, newtem):
     for i in range(len(template)):
@@ -470,6 +495,7 @@ def Update(template, newtem):
             template = tmp
 
     return template
+
 
 def Gen01fromSeq(seq, template):
     hap = list()
@@ -484,12 +510,14 @@ def Gen01fromSeq(seq, template):
 
     return hap
 
+
 def GetSeq(line):
     header = line.split()[0]
     line = line.replace(header, "")
     line = line.replace(" ", "")
     line = line.replace("\n", "")
     return line
+
 
 def GetSPN(Res):
     spn = []
@@ -504,9 +532,11 @@ def GetSPN(Res):
 
     return spn
 
+
 def CoalSPN(CurC, spn1, spn2):
     NewC = [spn1 + ',' + spn2] + [spn for spn in CurC if spn != spn1 and spn != spn2]
     return NewC
+
 
 def ProbSS(splist1, Res):
     prob = 1
@@ -524,6 +554,7 @@ def ProbSS(splist1, Res):
 
     return prob
 
+
 def ProbDS(splist1, splist2, Res):
     prob = 1
 
@@ -538,6 +569,7 @@ def ProbDS(splist1, splist2, Res):
                 prob *= Res[pair_key2][0]
 
     return prob
+
 
 def CompProb(CurC, Res):
     prob = 1
@@ -556,17 +588,18 @@ def CompProb(CurC, Res):
 
     return prob
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description = "CLADES (CLAssification based DElimitation of Species)")
-    
+    parser = argparse.ArgumentParser(description="CLADES (CLAssification based DElimitation of Species)")
+
     # Positional arguments
-    parser.add_argument("seq_data", type = Path, help = "The path to the sequence data file to be processed.")
-    
+    parser.add_argument("seq_data", type=Path, help="The path to the sequence data file to be processed.")
+
     # Optional argumentS
-    parser.add_argument("-p", "--model_path", type = Path, default = Path("model/"), help = "Path to model.")
-    parser.add_argument("-n", "--model_name", type = str, default = "All", help = "Name of the model.")
-    parser.add_argument("-t", "--total_sumstat", action = "store_true", help = "Outputs a single file containing all of the summary statistics on top of pairwise sumstat.")
-    parser.add_argument("-T", "--only_total_sumstat", action = "store_true", help = "Only outputs a single file containing all of the summary statistics. Does not output pairwise sumstat.")
+    parser.add_argument("-p", "--model_path", type=Path, default=Path("model/"), help="Path to model.")
+    parser.add_argument("-n", "--model_name", type=str, default="All", help="Name of the model.")
+    parser.add_argument("-t", "--total_sumstat", action="store_true", help="Outputs a single file containing all of the summary statistics on top of pairwise sumstat.")
+    parser.add_argument("-T", "--only_total_sumstat", action="store_true", help="Only outputs a single file containing all of the summary statistics. Does not output pairwise sumstat.")
 
     args = parser.parse_args()
 
@@ -581,7 +614,7 @@ if __name__ == "__main__":
     seq_data = args.seq_data
     model = args.model_path / args.model_name
     output_dir = seq_data.parent / "output/"
-    output_dir.mkdir(parents = True, exist_ok = True)
+    output_dir.mkdir(parents=True, exist_ok=True)
 
     # Step 1
     SS = compute_summary_statistics(seq_data)
